1e9e6c8fbfc89daf49bee64dee5cd47539838ad1
Guz Alexander <kalimatas@gmail.com>
2014-10-21 11:53:24 +0400

    New server try

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
@@ -9,23 +10,33 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.logging.Logger;
 
 public class Main {
-    public static void main(String... args) throws IOException, InterruptedException, ClassNotFoundException {
+    public static void main(String... args) {
+        int mode = 1;
+        if (args.length == 1) {
+            try {
+                mode = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                mode = 1;
+            }
+        }
+
         final GameServer gs = new GameServer();
 
-        (new Thread() {
-            public void run() {
-                try {
-                    gs.server();
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
-        gs.client();
+        switch (mode) {
+            case 1:
+                gs.client();
+                break;
+            case 2:
+                gs.server();
+                break;
+            default:
+                throw new RuntimeException("unknown mode");
+        }
     }
 }
 
@@ -59,17 +70,89 @@ class Packet implements Serializable {
 }
 
 class GameServer {
-    void client() throws IOException, ClassNotFoundException {
-        SocketChannel socketChannel = SocketChannel.open();
-        socketChannel.configureBlocking(false);
-        socketChannel.connect(new InetSocketAddress("127.0.0.1", 9090));
-        if (socketChannel.isConnectionPending()) {
-            socketChannel.finishConnect();
-        }
-        //while (!socketChannel.finishConnect()) {}
+    private static final int PORT = 4444;
+    private static final Logger LOGGER = Logger.getLogger(GameServer.class.getName());
+
+    private SocketChannel csc;
+
+    private List<SocketChannel> clients = new LinkedList<>();
+    private ServerSocketChannel ssc;
+    private Selector readSelector;
+
+    /**
+     * Client part
+     */
+    void client() {
+        LOGGER.info("starting client");
 
         try {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            csc = SocketChannel.open();
+            csc.configureBlocking(false);
+            csc.connect(new InetSocketAddress(InetAddress.getLoopbackAddress(), PORT));
+            while (!csc.finishConnect()) {}
+        } catch (IOException e) {
+            LOGGER.severe("cannot connect to server");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Server part
+     */
+    void server() {
+        LOGGER.info("starting server");
+
+        try {
+            ssc = ServerSocketChannel.open();
+            ssc.configureBlocking(false);
+            ssc.bind(new InetSocketAddress(InetAddress.getLoopbackAddress(), PORT));
+
+            readSelector = Selector.open();
+        } catch (IOException e) {
+            LOGGER.severe("cannot init server");
+            e.printStackTrace();
+        }
+
+        LOGGER.info("running server");
+        while (true) {
+            acceptNewConnections();
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void acceptNewConnections() {
+        SocketChannel clientChannel;
+        try {
+            while ((clientChannel = ssc.accept()) != null) {
+                LOGGER.info("got connection from " + clientChannel.socket().getInetAddress());
+
+                // add client to the list
+
+
+                // send broadcast
+                // send reply to the new client
+            }
+        } catch (IOException e) {
+            LOGGER.warning("error while accept()");
+            e.printStackTrace();
+        } catch (Exception e) {
+            LOGGER.warning("error in acceptNewConnections");
+            e.printStackTrace();
+        }
+    }
+
+    private void readMessages() {
+
+    }
+}
+
+/*
+ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
             ByteBuffer buf = ByteBuffer.allocate(2048);
             int bytesRead;
@@ -82,35 +165,15 @@ class GameServer {
             Packet packet = Packet.decode(baos.toByteArray());
             System.out.println("got: " + packet.get());
             System.out.println("got: " + packet.get());
+ */
 
-        } finally {
-            socketChannel.close();
-        }
-    }
-
-    void server() throws IOException, InterruptedException {
-        ServerSocketChannel ssc = ServerSocketChannel.open();
-        ssc.configureBlocking(false);
-        ssc.bind(new InetSocketAddress(9090));
-
-        while (true) {
-            SocketChannel socketChannel = ssc.accept();
-            if (socketChannel != null) {
-                System.out.println("got connection!");
-
-                Packet packet = new Packet();
+/*
+        Packet packet = new Packet();
                 packet.append("send from server");
                 packet.append(42);
 
                 socketChannel.write(ByteBuffer.wrap(Packet.encode(packet)));
-
-                Thread.sleep(500);
-                socketChannel.close();
-
-                break;
-            }
-            System.out.println("no client :(");
-        }
+        */
 
         /*
         Selector selector = Selector.open();
@@ -138,5 +201,3 @@ class GameServer {
             }
         }
         */
-    }
-}
